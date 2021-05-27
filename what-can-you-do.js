fa58c971ba2abd9b22a1247172c720ef6d448b80
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-27 12:23:04 -0600

    Clean and Comment Code - Final

--- what-can-you-do.js
+++ what-can-you-do.js
@@ -18,12 +18,16 @@
 
 const prompt = require('prompt-sync')();
 
+// use prompt to gather age
 const age = prompt('What is your age?');
 
+// Log out age | change age type to number from string |
+//log out type of numberAge | 
 console.log('You are ', age, ' years old. This means within our self applied social constrictions....');
 const numberAge = Number(age);
 console.log(typeof numberAge);
 
+// Test number against options to display requested result.
 if (numberAge < 16) {
     console.log("You can't drive! Or vote! Or Do Anything")
 } else if (numberAge > 15 && numberAge < 18) {
@@ -40,15 +44,4 @@ if (numberAge < 16) {
     console.log("Now go get your dancing shoes!");
 }
 
-// |
-// age | message |
-//     |
-//     -- - | -- - |
-//     |
-//     less than 16 | "You can't drive." |
-//     |
-//     16 to 17 | "You can drive but not vote." |
-//     |
-//     18 to 24 | "You can vote but not rent a car." |
-//     |
-//     25 or older | "You can do pretty much anything." |
+// ALL WORKING ABOVE THIS LINE
