d100c6c90415f1ad6bace9279e49c1ac4cad773b
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-27 12:50:27 -0600

    Add initial code

---
+++ group-sizes.js
@@ -0,0 +1,9 @@
+// ## `group-sizes.js`
+
+// Lets say we have a class of students that we want to separate into groups of three. Inevitably, if there are leftover students, we may need to have some groups of two. A group of one is not something that we want.
+
+// **Example 1:** if we have a class size of 15, we would create 5 groups of 3 and 0 groups of 2 
+// **Example 2:** if we have a class size of 16, we would create 4 groups of 3 and 2 groups of 2
+// **Example 3:** if we have a class size of 17, we would create 5 groups of 3 and 1 group of 2
+
+// Write a program that prompts the user for a class size, then logs out the number of groups of 3 the number of groups of 2 that the class would be divided into.
