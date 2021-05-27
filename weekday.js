77f409d3845beea895344db4552ddd1fd4f99cfb
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-27 12:45:14 -0600

    Test Code - Works"

--- weekday.js
+++ weekday.js
@@ -15,5 +15,37 @@ const PromptSync = require("prompt-sync");
 // | 7 | Sunday |
 // | anything else | "error" |
 
+
+// Get number from user and apply it to 'day'
 const prompt = require('prompt-sync')();
-const day = prompt('Pick a number any number! As long as it"s between 1 and 7')
+const day = prompt('Pick a number any number! As long as it"s between 1 and 7     ');
+
+//turn day into a number
+const numDay = Number(day);
+
+//Return the day of the week based on the number given.
+if (numDay === 1) {
+    console.log('1 | Monday')
+
+} else if (numDay === 2) {
+    console.log('2 | Tuesaday')
+
+} else if (numDay === 3) {
+    console.log('3 | Wednesday')
+
+} else if (numDay === 4) {
+    console.log('4 | Thursday')
+
+} else if (numDay === 5) {
+    console.log('5 | Friday')
+
+} else if (numDay === 6) {
+    console.log('6 | Saturday')
+
+} else if (numDay === 7) {
+    console.log('7 | Sunday')
+
+} else if (numDay > 7) {
+    console.log('error')
+
+};
