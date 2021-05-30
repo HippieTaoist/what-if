c18518d8b3b4ffd068f572fd22b10523ed94781e
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-05-29 20:41:20 -0600

    Add Iniital Instructional Code

---
+++ fizzbuzz.js
@@ -0,0 +1,22 @@
+// ##
+// `fizzbuzz.js`
+
+// This is a classic interview question.Write a program that asks the user
+// for a number.If the number is divisible by 3, the program logs "fizz".If the number is divisible by 5, log "buzz".If the number is divisible by 3 and 5, log "fizzbuzz".
+
+
+
+
+
+
+// ifx i divisalbe by 5 then  modol operator(%) should equal 0 when divdied by5 (same goes for 3)
+
+//next check if divisable by 3 && 5 
+
+const prompt = require('prompt-sync')();
+
+//get class size from user
+const bizzFuzz = prompt("Enter the number you have in you head... RIGHT NOW! GO!");
+
+const numBizzFuzz = Number(bizzFuzz)
+if ()
